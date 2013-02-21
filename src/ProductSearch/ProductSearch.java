5fc5a741f4f6447eb72503949f83a852e1d3e97d
Peter <peter-eliasson@hotmail.com>
2013-02-21 14:55:31 +0100

    Changed ProductSearch. Stores categories for results, and can also filter by multiple categories

--- src/ProductSearch/ProductSearch.java
+++ src/ProductSearch/ProductSearch.java
@@ -14,103 +14,133 @@ import java.util.*;
  */
 public class ProductSearch {
 
-	private List<Product> resultingProducts;
-	private String searchString;
-	private Comparator<Product> sortBy;
-	private ProductCategory categoryFilter;
+    private List<Product> resultingProducts;
+    private Set<ProductCategory> resultCategories;
+    private String searchString;
+    private Comparator<Product> sortBy;
+    private Set<ProductCategory> categoryFilter;
 
-	/**
-	 * Filters a list of products to include only the provided ProductCategory.
-	 * 
-	 * @param products
-	 * @param filter
-	 * @return
-	 */
-	private List<Product> filterProductsByCategory(List<Product> products,
-			ProductCategory filter) {
-		if (filter == null) {
-			return products;
-		} else {
-			List<Product> filteredProducts = new LinkedList<Product>();
-			for (Product p : products) {
-				if (p.getCategory() == filter) {
-					filteredProducts.add(p);
-				}
-			}
-			return filteredProducts;
-		}
-	}
+    /**
+     * Filters a list of products to include only those with a 
+     * ProductCategory in the provided ProductCategorySet.
+     * 
+     * @param products 
+     * @param filter
+     * @return
+     */
+    private List<Product> filterProductsByCategory(List<Product> products,
+            Set<ProductCategory> filter) {
+        if (filter == null) {
+            return products;
+        } else {
+            List<Product> filteredProducts = new LinkedList<Product>();
+            for (Product p : products) {
+                if (filter.contains(p.getCategory())) {
+                    filteredProducts.add(p);
+                }
+            }
+            return filteredProducts;
+        }
+    }
 
-	/**
-	 * Sorts a list of products by the provided Comperator.
-	 * 
-	 * @param products
-	 * @param sortBy
-	 * @return
-	 */
-	private List<Product> sortProductsBy(List<Product> products,
-			Comparator<Product> sortBy) {
+    /**
+     * Sorts a list of products by the provided Comperator.
+     * 
+     * @param products
+     * @param sortBy
+     * @return
+     */
+    private List<Product> sortProductsBy(List<Product> products,
+            Comparator<Product> sortBy) {
 
-		if (sortBy == null) {
-			return products;
-		}
+        if (sortBy == null) {
+            return products;
+        }
 
-		// No point in sorting less than two elements
-		if (products == null || products.size() < 2) {
-			return products;
-		}
+        // No point in sorting less than two elements
+        if (products == null || products.size() < 2) {
+            return products;
+        }
 
-		Collections.sort(products, sortBy);
+        Collections.sort(products, sortBy);
 
-		return products;
-	}
+        return products;
+    }
 
-	/**
-	 * Performs a search and saves the result.
-	 */
-	private void doSearch() {
-		IMatDataHandler dh = IMatDataHandler.getInstance();
+    /**
+     * Takes a list of products and returns the different categories.
+     * @param products
+     * @return a list of ProductCategorys contained in a list of products.
+     */
+    private Set<ProductCategory> getCategoriesFromProductList(List<Product> products) {
+        Set<ProductCategory> categories = new HashSet<ProductCategory>();
+        for (Product p : products) {
+            categories.add(p.getCategory());
+        }
+        return categories;
+    }
 
-		// Free text search
-		List<Product> searchResult = dh.findProducts(this.searchString);
+    /**
+     * Performs a search and saves the result.
+     */
+    private void doSearch() {
+        IMatDataHandler dh = IMatDataHandler.getInstance();
 
-		// Filter by category
-		searchResult = filterProductsByCategory(searchResult,
-				this.categoryFilter);
+        // Free text search
+        List<Product> searchResult = dh.findProducts(this.searchString);
 
-		// Sort results
-		searchResult = sortProductsBy(searchResult, this.sortBy);
+        // Filter by category
+        searchResult = filterProductsByCategory(searchResult,
+                this.categoryFilter);
 
-		this.resultingProducts = searchResult;
+        // Sort results
+        searchResult = sortProductsBy(searchResult, this.sortBy);
 
-	}
+        // Get the categories contained in the result
+        this.resultCategories = getCategoriesFromProductList(searchResult);
 
-	/**
-	 * Constructs a new search towards the IMatDataHandler backend.
-	 * 
-	 * @param searchString
-	 *            A case-insensitive search substring matched against names of
-	 *            the products.
-	 * @param sortBy
-	 *            An optional Comparator used for sorting the results. If null,
-	 *            the product list's default order will be preserved.
-	 * @param categoryFilter
-	 *            An optional ProductCategory that, if present, will only
-	 *            display results in this category. If null, the result will not
-	 *            be filtered by a category.
-	 */
-	public ProductSearch(String searchString, Comparator<Product> sortBy,
-			ProductCategory categoryFilter) {
-		// Null as search string is not accepted by the backend
-		this.searchString = (searchString == null) ? "" : searchString;
-		this.sortBy = sortBy;
-		this.categoryFilter = categoryFilter;
+        // Save the list of results
+        this.resultingProducts = searchResult;
 
-		doSearch();
-	}
+    }
 
-	public List<Product> getData() {
-		return this.resultingProducts;
-	}
+    /**
+     * Constructs a new search towards the IMatDataHandler backend.
+     * 
+     * @param searchString
+     *            A case-insensitive search substring matched against names of
+     *            the products.
+     * @param sortBy
+     *            An optional Comparator used for sorting the results. If null,
+     *            the product list's default order will be preserved.
+     * @param categoryFilter
+     *            An optional ProductCategory that, if present, will only
+     *            display results in this category. If null, the result will not
+     *            be filtered by a category.
+     */
+    public ProductSearch(String searchString, Comparator<Product> sortBy,
+            Set<ProductCategory> categoryFilter) {
+        // Null as search string is not accepted by the backend
+        this.searchString = (searchString == null) ? "" : searchString;
+        this.sortBy = sortBy;
+        this.categoryFilter = categoryFilter;
 
+        doSearch();
+    }
+
+    /**
+     * 
+     *
+     */
+    public List<Product> getSearchResult() {
+        return this.resultingProducts;
+    }
+
+    /**
+     * Returns the categories represented in the search result.
+     *
+     */
+    public Set<ProductCategory> getSearchCategories() {
+        return this.resultCategories;
+    }
 }
