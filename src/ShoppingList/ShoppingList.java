e3f6039598df0e641d3041c2a1cec20c715e3121
Peter <peter-eliasson@hotmail.com>
2013-02-26 19:31:57 +0100

    Adding basic file structure for ShoppingList

---
+++ src/ShoppingList/ShoppingList.java
@@ -0,0 +1,9 @@
+package ShoppingList;
+
+/**
+ * A Shopping list object, holding one or more ShoppingListItems.
+ * @author Peter
+ */
+public class ShoppingList {
+    
+}
---
+++ src/ShoppingList/ShoppingListsHandler.java
@@ -0,0 +1,11 @@
+package ShoppingList;
+
+/**
+ * A singelton class for handling the shopping lists of the user. 
+ * Provides access to all the user's shopping lists, and also handles
+ * saving and loading shopping lists to/from a file.
+ * @author Peter
+ */
+public enum ShoppingListsHandler {
+    
+}
